21828d913c414182f6e364ea2a4bde7db1c8088d
loubnamous <moussallamloubna@.com>
2025-02-10 22:36:25 -0500

    initial commit

---
+++ Modules/legoSets.js
@@ -0,0 +1,133 @@
+/********************************************************************************
+* WEB700 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Loubna Almoussallam Student ID: 114741242 Date: 30 Jan, 2025
+********************************************************************************/
+
+class LegoData{
+
+     sets;
+     constructor()
+     {
+        this.sets= [];
+     }
+
+     initialize()
+     {  
+      return new Promise((resolve,reject)=>{
+        const setDat = require("../data/setData"); 
+        const themeData = require("../data/themeData"); 
+
+        let i=0;
+        for(i=0;i<setDat.length;i++)
+        {
+            this.sets.push(setDat[i]);
+        }
+
+        this.sets.forEach(obj => {
+         const matchingProperties = themeData.find((theme) => theme.id === obj.theme_id);
+     
+         if (matchingProperties) {
+             obj.theme = matchingProperties.name; 
+             resolve('the sets array is filled with objects');
+         }
+         else{
+          reject('unable to fill the sets array!');
+         }
+       });
+
+      });
+
+     }
+
+     getAllSets()
+     {
+      return new Promise((resolve,reject)=>{
+        if(this.sets !=null)
+        {
+          resolve(this.sets);
+        }
+        else{
+          reject('No objects!');
+        }
+      });
+
+     }
+
+     getSetByNum(setNum)
+     {
+      return new Promise((resolve,reject)=>{
+        
+        const result = this.sets.find(({ set_num }) => set_num === setNum);
+        if(result != null)
+        {
+          resolve(result);
+        }
+        else
+        {
+          reject('Error with getSetByName: Unable to find requested set');
+        }
+     });
+       
+     }
+
+     getSetsByTheme(theme)
+     {
+      return new Promise((resolve,reject)=>{
+
+        const result1 = this.sets.filter((obj) => obj.theme.toLowerCase().includes(theme.toLowerCase()));
+        if(result1 != '')
+        {
+          resolve(result1);
+        }
+        else{
+          reject('Error with getSetsByTheme: Unable to find requested sets by theme');
+        }
+      });
+     }
+
+    
+
+}
+module.exports = LegoData;
+// let data = new LegoData();
+
+
+
+// async function executePromises() {
+ 
+//   try {
+//     let resultInit = await data.initialize();
+//     console.log(resultInit);
+
+//     let resultGetAll = await data.getAllSets();
+//     console.log(resultGetAll);
+
+//     try {
+//         let resultGetByName = await data.getSetByNum("0012-1w");
+//         console.log(resultGetByName);
+//     } catch (err) {
+//         console.error(err);
+//     }
+
+//     try {
+//         let resultGetByTheme = await data.getSetsByTheme('tech');
+//         console.log(resultGetByTheme);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// } catch (err) {
+//     console.log(err);
+// }
+// }
+
+// console.log(executePromises());
+
+// console.log(`Number of Sets: ${data.getAllSets().length}`);
+// console.log(data.getSetByNum("0012-1"));
+// console.log(`Number of 'tech' sets: ${data.getSetsByTheme('tech').length}`);
---
+++ server.js
@@ -0,0 +1,76 @@
+const LegoData = require("./Modules/legoSets");
+const legoData = new LegoData();
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
+require('pg'); // explicitly require the "pg" module
+const Sequelize = require('sequelize');
+
+legoData.initialize()
+  .then(() => {
+    console.log("Initialization successful");
+    app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+
+  })
+  .catch((error) => console.error(error));
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Views/home.html'));
+  });
+
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+  });
+
+  app.get('/lego/sets', async (req, res) => {
+    let sets;
+    try {
+        const theme = req.query.theme; 
+    
+        
+        if (theme) {
+          sets = await legoData.getSetsByTheme(theme);
+          res.send(sets);
+          
+        } else {
+          sets = await legoData.getAllSets();
+          res.send(sets);
+        }
+    
+      } catch (error) {
+      
+        res.sendFile(path.join(__dirname, '/views/404.html'));
+
+      }
+    
+  });
+
+  app.get('/lego/sets/:set_num', async (req, res) => {
+    let sets;
+    try {
+
+        const num = req.params.set_num; 
+        
+        
+        
+          sets = await legoData.getSetByNum(num);
+          res.send(sets);
+          
+        
+    
+      } catch (error) {
+      
+        res.sendFile(path.join(__dirname, '/views/404.html'));
+
+      }
+    
+  });
