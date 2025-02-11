a17b656ac064bb2b064e864aacd77d0874b4f2de
ammarsalmawy <ammar.salmawy@ontariotechu.net>
2025-02-11 12:09:21 -0500

    update1

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/public'));
 
-require('pg'); // explicitly require the "pg" module
-const Sequelize = require('sequelize');
+// require('pg'); // explicitly require the "pg" module
+// const Sequelize = require('sequelize');
 
 legoData.initialize()
   .then(() => {
